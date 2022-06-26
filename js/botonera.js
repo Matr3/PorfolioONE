3afc908f6709dd557459085110a8c76f1b43a1ac
Ma_tre3 <tremari.marce@gmail.com>
2022-06-26 13:42:22 -0300

    Version Desktop

---
+++ js/botonera.js
@@ -0,0 +1,334 @@
+function animationHome(){
+  document.querySelector(".presentacion").style.animation = "fadeOuputTop ease 2s backwards";
+  document.querySelector(".presentacion1").style.animation = "fadeOuputTop ease 2s backwards";
+  document.querySelector(".presentacion2").style.animation = "fadeOuputTop ease 2s backwards";
+  document.querySelector(".comentario").style.animation = "fadeOuputRight ease 2s backwards";
+  document.querySelector(".contenidoPrincipal_link__1").style.animation = "animate 3s linear infinite, rotate ease 2s backwards";
+  document.querySelector(".contenidoPrincipal_link__2").style.animation = "animate 3s linear infinite, rotate ease 2s backwards";
+  document.querySelector(".contenidoPrincipal_link__3").style.animation = "animate 3s linear infinite, rotate ease 2s backwards";
+  document.querySelector(".imgEmpresarial").style.animation = "-imgRotate 5s backwards";
+}
+
+function animationSobreMi(){
+  document.querySelector(".sobreMi").style.animation = "fadeOuputRight ease 2s backwards";
+}
+
+function animationSkills(){
+  document.querySelector(".skills").style.animation = "fadeOuputRight ease 2s backwards";
+}
+
+function animationHobbies(){
+  document.querySelector(".hobbies").style.animation = "fadeOuputRight ease 2s backwards";
+}
+
+function animationFormacion(){
+  document.querySelector(".formacion").style.animation = "fadeOuputRight ease 2s backwards";
+}
+
+function animationProyectos(){
+  document.querySelector(".proyectos").style.animation = "fadeOuputRight ease 2s backwards";
+}
+
+function animationContactos(){
+  document.querySelector(".contacto").style.animation = "fadeOuputRight ease 2s backwards";
+}
+
+function aimationMenuFadeIn(){
+  document.querySelector(".headerPrincipal").style.animation = "fadeIn ease 3s backwards";
+}
+
+function aimationMenuFadeOuput(){
+  document.querySelector(".headerPrincipal").style.animation = "fadeOuput ease 3s backwards";
+}
+
+var btSobreMi = document.querySelector("#btSobreMi");
+btSobreMi.addEventListener("click", function(){
+  aimationMenuFadeOuput();
+  animationHome();
+  document.querySelector(".sobreMi").style.animation = "fadeInLeft ease 2s backwards";
+  animationSkills();
+  animationHobbies();
+  animationFormacion();
+  animationProyectos();
+  animationContactos();
+var timeLeft = 10; 
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+    if (timeLeft == -1) {
+        clearTimeout(timerId);
+       
+    } else {
+      if(timeLeft==9)
+      {
+         muestraDiv();
+      }  
+      if(timeLeft==8){
+        aimationMenuFadeIn();
+        document.querySelector(".sobreMi").style.display = "flex";
+        document.querySelector(".social").style.display = "flex";
+        document.querySelector(".contenidoPrincipal_link__1").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+        document.querySelector(".contenidoPrincipal_link__2").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+        document.querySelector(".contenidoPrincipal_link__3").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+      }      
+      
+        timeLeft--;
+    }
+}
+
+function muestraDiv() {
+  document.querySelector(".principal").style.display = "none";
+    document.querySelector(".skills").style.display = "none";
+    document.querySelector(".hobbies").style.display = "none";
+    document.querySelector(".social").style.display = "none";
+    document.querySelector(".formacion").style.display = "none";
+    document.querySelector(".proyectos").style.display = "none";
+    document.querySelector(".contacto").style.display = "none";
+}
+});
+
+
+var btSkills = document.querySelector("#btSkills");
+btSkills.addEventListener("click", function(){
+  aimationMenuFadeOuput();
+  animationHome();
+  document.querySelector(".skills").style.animation = "fadeInLeft ease 2s backwards";
+  animationSobreMi();
+  animationHobbies();
+  animationFormacion();
+  animationProyectos();
+  animationContactos();
+  var timeLeft = 10; 
+  var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+    if (timeLeft == -1) {
+        clearTimeout(timerId);
+       
+    } else {
+      if(timeLeft==9)
+      {
+         muestraDiv();
+      }  
+      if(timeLeft==8){
+        aimationMenuFadeIn();
+        document.querySelector(".skills").style.display = "flex";
+        document.querySelector(".social").style.display = "flex";
+        document.querySelector(".contenidoPrincipal_link__1").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+        document.querySelector(".contenidoPrincipal_link__2").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+        document.querySelector(".contenidoPrincipal_link__3").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+      }      
+      
+        timeLeft--;
+    }
+}
+
+function muestraDiv() {
+  document.querySelector(".principal").style.display = "none";
+    document.querySelector(".sobreMi").style.display = "none";
+    document.querySelector(".hobbies").style.display = "none";
+    document.querySelector(".social").style.display = "none";
+    document.querySelector(".formacion").style.display = "none";
+    document.querySelector(".proyectos").style.display = "none";
+    document.querySelector(".contacto").style.display = "none";
+}
+});
+
+var btHobbies = document.querySelector("#btHobbies");
+btHobbies.addEventListener("click", function(codificacion){
+    codificacion.preventDefault();
+    aimationMenuFadeOuput();
+    animationHome();
+    animationSobreMi();
+    animationSkills();
+    animationFormacion();
+    animationProyectos();
+    animationContactos();
+    document.querySelector(".hobbies").style.animation = "fadeInLeft ease 2s backwards";
+    
+  
+    var timeLeft = 10; 
+    var timerId = setInterval(countdown, 1000);
+  
+  function countdown() {
+      if (timeLeft == -1) {
+          clearTimeout(timerId);
+         
+      } else {
+        if(timeLeft==9)
+        {
+           muestraDiv();
+        }  
+        if(timeLeft==8){
+          aimationMenuFadeIn();
+          document.querySelector(".hobbies").style.display = "flex";
+          document.querySelector(".social").style.display = "flex";
+          document.querySelector(".contenidoPrincipal_link__1").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+          document.querySelector(".contenidoPrincipal_link__2").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+          document.querySelector(".contenidoPrincipal_link__3").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+        }      
+        
+          timeLeft--;
+      }
+  }
+  function muestraDiv() {
+    document.querySelector(".principal").style.display = "none";
+      document.querySelector(".sobreMi").style.display = "none";
+      document.querySelector(".skills").style.display = "none";
+      document.querySelector(".social").style.display = "none";
+      document.querySelector(".formacion").style.display = "none";
+      document.querySelector(".proyectos").style.display = "none"; 
+      document.querySelector(".contacto").style.display = "none";
+  }
+});
+
+
+  var btFormacion = document.querySelector("#btFormacion");
+  btFormacion.addEventListener("click", function(codificacion){
+      codificacion.preventDefault();
+      aimationMenuFadeOuput();
+      animationHome();
+      animationSobreMi();
+      animationSkills();
+      animationHobbies();
+      animationProyectos();
+      animationContactos();
+      document.querySelector(".formacion").style.animation = "fadeInLeft ease 2s backwards";
+
+
+      var timeLeft = 10; 
+    var timerId = setInterval(countdown, 1000);
+  
+  function countdown() {
+      if (timeLeft == -1) {
+          clearTimeout(timerId);
+         
+      } else {
+        if(timeLeft==9)
+        {
+           muestraDiv();
+        }  
+        if(timeLeft==8){
+          aimationMenuFadeIn();
+          document.querySelector(".formacion").style.display = "flex";
+          document.querySelector(".social").style.display = "flex";
+          document.querySelector(".contenidoPrincipal_link__1").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+          document.querySelector(".contenidoPrincipal_link__2").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+          document.querySelector(".contenidoPrincipal_link__3").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+        }      
+        
+          timeLeft--;
+      }
+  }
+  function muestraDiv() {
+    document.querySelector(".principal").style.display = "none";
+      document.querySelector(".sobreMi").style.display = "none";
+      document.querySelector(".skills").style.display = "none";
+      document.querySelector(".social").style.display = "none";
+      document.querySelector(".hobbies").style.display = "none";
+      document.querySelector(".proyectos").style.display = "none";
+      document.querySelector(".contacto").style.display = "none";
+  }
+
+    });
+
+
+    var btProyectos = document.querySelector("#btProyectos");
+    btProyectos.addEventListener("click", function(codificacion){
+        codificacion.preventDefault();
+        aimationMenuFadeOuput();
+        animationHome();
+        animationSobreMi();
+        animationSkills();
+        animationHobbies();
+        animationFormacion();
+        animationContactos();
+        document.querySelector(".proyectos").style.animation = "fadeInLeft ease 2s backwards";
+  
+  
+        var timeLeft = 10; 
+      var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+        if (timeLeft == -1) {
+            clearTimeout(timerId);
+           
+        } else {
+          if(timeLeft==9)
+          {
+             muestraDiv();
+          }  
+          if(timeLeft==8){
+            aimationMenuFadeIn();
+            document.querySelector(".proyectos").style.display = "flex";
+            document.querySelector(".social").style.display = "flex";
+            document.querySelector(".contenidoPrincipal_link__1").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+            document.querySelector(".contenidoPrincipal_link__2").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+            document.querySelector(".contenidoPrincipal_link__3").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+          }      
+          
+            timeLeft--;
+        }
+    }
+    function muestraDiv() {
+      document.querySelector(".principal").style.display = "none";
+        document.querySelector(".sobreMi").style.display = "none";
+        document.querySelector(".skills").style.display = "none";
+        document.querySelector(".social").style.display = "none";
+        document.querySelector(".hobbies").style.display = "none";
+        document.querySelector(".formacion").style.display = "none";
+        document.querySelector(".contacto").style.display = "none";
+    }
+  
+      });
+
+
+var btContacto = document.querySelector("#btContacto");
+btContacto.addEventListener("click", function(codificacion){
+          codificacion.preventDefault();
+          aimationMenuFadeOuput();
+          animationHome();
+          animationSobreMi();
+          animationSkills();
+          animationHobbies();
+          animationFormacion();
+          animationProyectos();
+          document.querySelector(".contacto").style.animation = "fadeInLeft ease 2s backwards";
+    
+    
+          var timeLeft = 10; 
+        var timerId = setInterval(countdown, 1000);
+      
+      function countdown() {
+          if (timeLeft == -1) {
+              clearTimeout(timerId);
+             
+          } else {
+            if(timeLeft==9)
+            {
+               muestraDiv();
+            }  
+            if(timeLeft==8){
+              aimationMenuFadeIn();
+              document.querySelector(".contacto").style.display = "flex";
+              document.querySelector(".social").style.display = "flex";
+              document.querySelector(".contenidoPrincipal_link__1").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+              document.querySelector(".contenidoPrincipal_link__2").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+              document.querySelector(".contenidoPrincipal_link__3").style.animation = "animate 3s linear infinite, -rotate ease 1s backwards";
+            }      
+            
+              timeLeft--;
+          }
+      }
+      function muestraDiv() {
+        document.querySelector(".principal").style.display = "none";
+          document.querySelector(".sobreMi").style.display = "none";
+          document.querySelector(".skills").style.display = "none";
+          document.querySelector(".social").style.display = "none";
+          document.querySelector(".hobbies").style.display = "none";
+          document.querySelector(".formacion").style.display = "none";
+          document.querySelector(".proyectos").style.display = "none";
+          
+      }
+    
+        });
---
+++ js/validacion.js
@@ -0,0 +1,129 @@
+const inputs = document.querySelectorAll('input, textarea')
+const enviar = document.querySelector('[name="enviar"]')
+
+inputs.forEach(input => {
+    input.addEventListener('blur', (evento) => {
+        valida(evento.target)
+        verificaCamposVacios()
+    })
+})
+
+function valida(input) {
+    const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+}
+
+function verificaCamposVacios() {
+    let campoNombre = document.querySelector("[name='nombre']")
+    let campoEmail = document.querySelector("[name='email']")
+    let campoAsunto = document.querySelector("[name='asunto']")
+    let campoMensaje = document.querySelector("[name='mensaje']")
+
+    if (campoNombre.value != '' && campoEmail.value != '' && campoAsunto.value != ''&& campoMensaje.value != '') {
+        enviar.disabled = false
+    } else {
+        enviar.disabled = true
+    }
+
+}
+
+const validadores = {
+    nombre:input => verificaNombre(input),
+    email:input => verificaEmail(input),
+    asunto:input => verificaAsunto(input),
+    mensaje:input => verificaMensagem(input)
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagensDeErro = {
+    nombre: {
+        valueMissing: 'El campo nombre no debe estar vacío.',
+        customError: 'El campo no debe contener más de 50 caracteres.',
+    },
+    email: {
+        valueMissing: 'El campo e-mail no puede estar vacío.',
+        customError: 'Campo inválido',
+    },
+    asunto: {
+        valueMissing: 'El campo asunto no puede estar vacío.',
+        customError: 'El campo no debe contener más de 50 caracteres'
+    },
+    mensaje: {
+        valueMissing: 'El campo mensaje no puede estar vacío',
+        customError: 'El mensaje no debe contener más de 300 caracteres'
+    }
+}
+
+function mostraMensajeDeErro(tipoDeInput, input) {
+    let mensaje = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensaje = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    
+    return mensaje
+}
+
+function verificaNombre(input) {
+    let mensaje = ''
+    if (input.value.length >= 50){
+        mensaje = 'El campo no debe contener más de 50 caracteress.'
+    }
+    if (!input.value.length){
+        mensaje = 'Este campo no puede estar vacio'
+    }
+
+    input.setCustomValidity(mensaje)
+}
+
+function verificaEmail(input) {
+    let mensaje = ''
+    let mailRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')
+
+    if (!mailRegex.test(input.value)){
+        mensaje = 'el formato del e-mail es inválido'
+    }
+    if (!input.value.length){
+        mensaje = 'Este campo no puede estar vacio'
+    }
+
+    input.setCustomValidity(mensaje)
+    
+}
+
+function verificaAsunto(input) {
+    let mensaje = ''
+    if (input.value.length >= 50){
+        mensaje = 'El campo no debe contener más de 50 caracteres'
+    }
+
+    if (!input.value.length) {
+        mensaje = 'Este campo no puede estar vacio.'
+    }
+
+    input.setCustomValidity(mensaje)
+}
+
+function verificaMensagem(input) {
+    let mensaje = ''
+
+    if (input.value.length >= 300){
+        mensaje = 'Este campo no permite mas de 300 caracteres'
+    }
+
+    if (!input.value.length) {
+        mensaje = 'Este campo no puede estar vacio'
+    }
+
+    input.setCustomValidity(mensaje)
+}
